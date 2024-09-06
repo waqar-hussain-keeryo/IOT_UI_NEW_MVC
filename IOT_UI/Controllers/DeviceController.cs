c96bb91d9af30dc73d0a37cba3a92f7df7d5c621
Waqar Hussain <waqarhussainkeeryo@hotmail.com>
2024-09-06 11:57:44 +0500

    Add device list

---
+++ IOT_UI/Controllers/DeviceController.cs
@@ -0,0 +1,59 @@
+﻿using IOT_UI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace IOT_UI.Controllers
+{
+    public class DeviceController : BaseController
+    {
+        public DeviceController(HttpClient httpClient, IConfiguration configuration) : base(httpClient, configuration) { }
+
+        private IActionResult RedirectToLoginIfNeeded()
+        {
+            var token = HttpContext.Session.GetString("JWTtoken");
+            if (string.IsNullOrEmpty(token))
+            {
+                return Redirect("~/Login/Index");
+            }
+            return null;
+        }
+
+        public async Task<IActionResult> Index(Guid siteId)
+        {
+            var redirectResult = RedirectToLoginIfNeeded();
+            if (redirectResult != null)
+            {
+                return redirectResult;
+            }
+
+            // Store customerId in session
+            HttpContext.Session.SetString("siteId", siteId.ToString());
+
+            var device = await GetDeviceBySiteId(siteId);
+            ViewBag.siteId = siteId;
+            return View(device);
+        }
+
+        [HttpGet]
+        public async Task<List<Device>> GetDeviceBySiteId(Guid siteId)
+        {
+            SetAuthorizationHeader();
+            var url = $"{_configuration["ApiBaseUrl"]}Customer/GetSiteDevices?siteId={siteId}";
+
+            HttpResponseMessage response = await _httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = await response.Content.ReadAsStringAsync();
+                var apiResponse = JsonConvert.DeserializeObject<ApiResponse<List<Device>>>(data);
+
+                if (apiResponse != null && apiResponse.Success)
+                {
+                    return apiResponse.Data;
+                }
+            }
+
+            return new List<Device>();
+        }
+    }
+}
