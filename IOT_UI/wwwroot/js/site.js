f876a53ce00d5dc002355c87ea312c3d4a5b5cb8
Waqar Hussain <waqarhussainkeeryo@hotmail.com>
2024-09-30 16:45:53 +0500

    Update code

--- IOT_UI/wwwroot/js/site.js
+++ IOT_UI/wwwroot/js/site.js
@@ -2,9 +2,4 @@
     const sidebar = document.getElementById('sidebar');
     sidebar.classList.toggle('expand');
     const mainContent = document.querySelector('.main');
-    if (sidebar.classList.contains('expand')) {
-        mainContent.style.marginLeft = '260px'; // Adjust margin for expanded sidebar
-    } else {
-        mainContent.style.marginLeft = '70px'; // Reset margin for collapsed sidebar
-    }
 });
